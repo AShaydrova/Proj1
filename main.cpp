ac9b86b6a8c675ceae6a8e96555917e936a7c97d
Anastasia <shaydrova08@gmail.com>
2022-03-12 23:48:46 +0800

    Stack

--- main.cpp
+++ main.cpp
@@ -1,8 +1,31 @@
-#include <QCoreApplication>
+#include "Exceptions.h"
+#include "TStack.h"
 
-int main(int argc, char *argv[])
+
+int main()
 {
-    QCoreApplication a(argc, argv);
+    Stack<int> S0; //Создание стека S0
+    for (int i = 1; i < 5; i++) {
+        S0.Push(i); //помещение объекта в стек S0
+    }
+    Stack<int> S1(S0); //используется конструктор копирования
+    for (int i = 5; i < 8; i++) {
+        S1.Push(i); //помещение объекта в стек S1
+    }
+    S0 = S1; //используется оператор копирования
+    std::cout << "Stack dimension: " << S0.StackDim() << std::endl; //вывод размера стека
 
-    return a.exec();
+    while(1){
+        try // Критический участок. Возможны исключительные ситуации
+        {
+            std::cout << S0.Pop() << std::endl; //извлечение объекта из стека
+        }
+        catch (const exc::EStackException& e) { // обрабатываем исключение
+            std::cout << "Exception: " << e.what() << std::endl;
+            break;
+        }
+    }
+    S1.Empty();
+
+    return 0;
 }
